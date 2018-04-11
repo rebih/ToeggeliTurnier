e160ee2511f5d34629e2fc449b91e08a0e4e6a69
lucrue <luca.rueegger@students.fhnw.ch>
2018-04-11 21:55:11 +0200

    added click function to button "Neuer Spieler" to open PlayerPage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { PlayerPage } from '../player/player'
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,4 +11,7 @@ export class HomePage {
 
   }
 
+  OpenPlayerPage(){
+    this.navCtrl.setRoot(PlayerPage);
+  }
 }
